12aa286de4349c750672820b331b51825d2eec0d
NestorMaP <mateoprats.nestor@gmail.com>
2024-04-26 19:29:27 +0200

    Add UD10 A1

---
+++ NetBeansProjects/UD10/src/ud10/Ejercicio_A1.java
@@ -0,0 +1,64 @@
+package ud10;
+
+import java.io.*;
+import java.util.Scanner;
+/**
+ *
+ * @author NestorMaP
+ */
+public class Ejercicio_A1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner reader = new Scanner(System.in);
+        String rutaIntroducida ="";
+        // Mientras la ruta introducida sea distinta de vacía (tecla intro)
+        do {
+            try {
+                System.out.print("Introduce la ruta (intro para salir): ");
+                rutaIntroducida=reader.nextLine();
+                
+                if(!rutaIntroducida.equals("")) {
+                    File f = new File(rutaIntroducida);
+                    muestraInfoRuta(f);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
+            }
+        } while (!rutaIntroducida.equals(""));
+        
+        System.out.println("Fin del programa");
+        
+    }
+    
+    public static void muestraInfoRuta(File ruta) throws FileNotFoundException {
+        // Si la ruta no existe lanza una excepción
+        if (!ruta.exists()) {
+            throw new FileNotFoundException("La ruta introducida no existe");
+        }
+        // Si la ruta es un archivo muestra el nombre del archivo
+        if (ruta.isFile()) {
+            System.out.println("Nombre del archivo: " + ruta.getName());
+        } else if (ruta.isDirectory()) //Si la ruta es carpeta primero muestra lista de carpetas y después archivos.
+        {
+            File[] lista = ruta.listFiles();
+            // Recorre la lista y muestra las carpetas
+            for (int i=0;i<lista.length;i++) {
+                if (lista[i].isDirectory()) {
+                    System.out.println("[*] " + lista[i].getName());
+                }
+            }    
+            // Recorre la lista y muestra los archivos    
+            for (int i=0;i<lista.length;i++) {
+                if (lista[i].isFile()) {
+                    System.out.println("[A] " + lista[i].getName());
+                }
+            }
+        }
+        
+        System.out.println("");
+    }
+}   
---
+++ NetBeansProjects/UD10/src/ud10/UD10.java
@@ -0,0 +1,19 @@
+package ud10;
+
+/**
+ *
+ * @author NestorMaP
+ * @version 26/04/2024
+ */
+public class UD10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
