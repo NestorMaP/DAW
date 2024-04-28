03b8b0949b7b902b3d12f711e7a189e2ada7e861
NestorMaP <mateoprats.nestor@gmail.com>
2024-04-28 20:09:14 +0200

    Improvement MiniLinuxTerminal

--- NetBeansProjects/MiniTerminal/src/miniterminal/MiniFileManager.java
+++ NetBeansProjects/MiniTerminal/src/miniterminal/MiniFileManager.java
@@ -9,9 +9,8 @@ import java.util.*;
 
 public class MiniFileManager {
     // Variables
-    private String ruta = "";
-    final String linuxBegin = "user@machine:";
-    File f;
+    private String ruta;
+    private String currentAddress = "user@machine:" + ruta + "$ ";
     
     // Getters y Setters
     public String getRuta() {
@@ -20,9 +19,12 @@ public class MiniFileManager {
     public void setRuta(String ruta) {
         this.ruta = ruta;
     }
+    public String getCurrentAddress() {
+        return this.currentAddress;
+    }
     // Constructor
     public MiniFileManager () {
-        f = new File(ruta);
+        this.setRuta("/");
     }
     
     // METHODS//
@@ -33,6 +35,7 @@ public class MiniFileManager {
      */
     // CAMBIAR ESTO PARA QUE NO SALGA NULL
     public void showFolder() {
+        File f = new File(ruta);
         if(f.isDirectory()){
             System.out.println(f.getParent() + "/" + f.getName());
         } else {
@@ -40,18 +43,133 @@ public class MiniFileManager {
         }
     }
     /**
-     * (cd <DIR>) Change address
+     * (cd) Change address
      * @param newAddress new Address
      */
     public void changeAddress (String newAddress) {
+        File f = new File(ruta);
         if(newAddress.equals("..")) {
             this.setRuta(f.getParent());
-            f = new File(this.getRuta());
         } else {
             this.setRuta(newAddress);
-            f = new File (this.getRuta());
         }
     }
     
+    /**
+     * (ls) Prints the folders and files
+     * @throws FileNotFoundException 
+     */
+    public void showDirFiles() throws FileNotFoundException{
+        File f = new File(ruta);
+        if(!f.exists()) {
+            throw new FileNotFoundException("La ruta actual es incorrecta.");
+        }
+        //Create vector with files and folders in the address
+        File[] list = f.listFiles();
+        //Sort the vector
+        Arrays.sort(list);
+        // Iterates the list and prints the folders
+        for (File fi : list) {
+            if (fi.isDirectory()) {
+                System.out.println("[*] " + fi.getName());
+            }
+        }    
+        // Iterates the list and prints the files
+        for (File fi : list) {
+            if (fi.isFile()) {
+                System.out.println("[A] " + fi.getName());
+            }
+        }
+    }
+    
+    /**
+     * (ll) Prints the folders and files with info
+     * @throws FileNotFoundException 
+     */
+    public void showDirFilesAndInfo() throws FileNotFoundException{
+        File f = new File(ruta);
+        if(!f.exists()) {
+            throw new FileNotFoundException("La ruta actual es incorrecta.");
+        }
+        //Create vector with files and folders in the address
+        File[] list = f.listFiles();
+        //Sort the vector
+        Arrays.sort(list);
+        //Iterates the list and prints the folders
+        for (File fi : list) {
+            if (fi.isDirectory()) {
+                System.out.println("[*] " + fi.getName() + "\t" 
+                    + fi.length() + " bytes\t" + new Date(fi.lastModified()));
+            }
+        }    
+        //Iterates the list and prints the files
+        for (File fi : list) {
+            if (fi.isFile()) {
+                System.out.println("[A] " + fi.getName() + "\t" 
+                    + fi.length() + " bytes\t" + new Date(fi.lastModified()));
+            }
+        }
+    }
+    /**
+     * (mkdir) Makes new directory
+     * @param dir String with address
+     * @throws FileNotFoundException 
+     */
+    public void makeDirectory(String dir) throws FileNotFoundException {
+        File f = new File(ruta + "/" + dir);
+        if (!f.exists()) {
+            throw new FileNotFoundException("La ruta actual es incorrecta.");
+        }
+        f.mkdir();
+    }
+    /**
+     * (rm) Deletes file or folder
+     * @param file
+     * @throws FileNotFoundException 
+     */
+    public void removeFile(String file) throws FileNotFoundException {
+        File f = new File(ruta + "/" + file);
+        if (!f.exists()) {
+            throw new FileNotFoundException("El archivo no se ha encontrado.");
+        }
+        // If it is a file, we'll delete it
+        if (f.isFile()) {
+            f.delete();
+        }
+        if (f.isDirectory()) {
+            for (File fl:f.listFiles()) {
+                if (fl.isFile()){
+                    fl.delete();
+                }
+                if (fl.isDirectory()) {
+                    System.err.println("Se está intentando borrar una subcarpeta.");
+                    break;
+                }
+            }
+            f.delete();
+        }
+    }
+    
+    public void moveOrRename(String File1, String File2) {
+        File f1 = new File(ruta + "/" + File1);
+        File f2 = new File(ruta + "/" + File2);
+        
+        f1.renameTo(f2);
+    }
+    
+    public void showHelp() {
+        System.out.println(
+                "Encuentre la lista de comenados a continuación\n" +
+                "----------------------------------------------\n" +
+                "pwd:\t\t\t" + "Muestra la carpeta actual.\n" +  
+                "cd <DIR>:\t\t" + "Cambia carpeta actual a 'DIR'.\n" +  
+                "ls:\t\t\t" + "Lista los directorios y archivos.\n" +  
+                "ll:\t\t\t" + "Lista con información los directorios y archivos.\n" +  
+                "mkdir <DIR>:\t\t" + "Crea el directorio 'DIR'.\n" +  
+                "rm <FILE>:\t\t" + "Borra 'FILE'.\n" +  
+                "mv <FILE1><FILE2>:\t" + "Mueve o renombra 'FILE1' a 'FILE2'.\n" +   
+                "help:\t\t\t" + "Muestra la lista de comandos.\n" +   
+                "exit:\t\t\t" + "Finaliza el programa.\n");
+    }
     
 }
--- NetBeansProjects/MiniTerminal/src/miniterminal/MiniTerminal.java
+++ NetBeansProjects/MiniTerminal/src/miniterminal/MiniTerminal.java
@@ -4,6 +4,10 @@ package miniterminal;
  *
  * @author NéstorMaP
  */
+
+import java.util.*;
+import java.io.*;
+
 public class MiniTerminal {
 
     /**
@@ -12,10 +16,78 @@ public class MiniTerminal {
     public static void main(String[] args) {
         
         MiniFileManager mfm = new MiniFileManager();
-        mfm.showFolder();
-        mfm.changeAddress("Documentos/nestor");
-        mfm.showFolder();
+        String[] userInputs;
+        String option = "";
+        
+        Scanner keyb = new Scanner(System.in);
+        
+        do {
+            
+            try {
+                
+                System.out.print(mfm.getCurrentAddress());
+                userInputs = keyb.nextLine().split(" ");
+                option = userInputs[0];
+                
+                switch(option) {
+                    case "pwd" : 
+                        
+                    break;
+                    
+                    
+                    case "cd" : 
+                        
+                    break;
+                    
+                    
+                    case "ls" : 
+                        mfm.showDirFiles();
+                    break;
+                    
+                    
+                    case "ll" : 
+                        mfm.showDirFilesAndInfo();
+                    break;
+                    
+                    
+                    case "mkdir" : 
+                        
+                    break;
+                    
+                    
+                    case "rm" : 
+                        
+                    break;
+                    
+                    
+                    case "mv" : 
+                        
+                    break;
+                    
+                    
+                    case "help" : 
+                        mfm.showHelp();
+                    break;
+                    
+                    
+                    case "exit" : 
+                        System.out.println("Se finaliza el programa.");
+                    break;
+                    
+                    
+                }
+                
+                
+                
+            } catch (FileNotFoundException e) {
+                System.out.println("ERROR: " + e);
+            } catch (Exception e) {
+                System.out.println("ERROR: " + e);
+            } 
+            
+        } while (!option.equalsIgnoreCase("exit"));
         
     }
+    
 
 }
