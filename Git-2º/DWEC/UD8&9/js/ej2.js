d801473c3c9fad1dcbf0dc029764eeef566017d3
NestorMaP <mateoprats.nestor@gmail.com>
2024-11-10 14:16:20 +0100

    DWEC UD8 Act 123

--- Git-2º/DWEC/UD8&9/js/ej2.js
+++ Git-2º/DWEC/UD8&9/js/ej2.js
@@ -1,12 +1,11 @@
-let axisX = document.getElementById("x_pos");
-let axisY = document.getElementById("y_pos");
+let axisX = document.getElementById('x_pos');
+let axisY = document.getElementById('y_pos');
 
 function updatePosition(event) {
-    let x = event.clientX;
-    let y = event.clientY;
-    axisX.textContent = `Axis X: ${x}`;
-    axisY.textContent = `Axis Y: ${y}`;
+  let x = event.clientX;
+  let y = event.clientY;
+  axisX.textContent = `Axis X: ${x}`;
+  axisY.textContent = `Axis Y: ${y}`;
 }
 
-
-document.addEventListener("mousemove", updatePosition);
+document.addEventListener('mousemove', updatePosition);
