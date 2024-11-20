38cbb618d2a6fc09b44869d2beb4d6d1e9887769
NestorMaP <mateoprats.nestor@gmail.com>
2024-11-20 18:35:37 +0100

    DWEC UD10 EJ3,5

--- Git-2º/DWEC/UD10/js/ej3.js
+++ Git-2º/DWEC/UD10/js/ej3.js
@@ -1,28 +1,73 @@
-let elements = document.querySelectorAll('.elem');
-let mainBtn = document.getElementById('main_btn');
+let prom1 = document.getElementById('prom1');
+let prom2 = document.getElementById('prom2');
+let prom3 = document.getElementById('prom3');
+let prom4 = document.getElementById('prom4');
 
-// To hide an element
-function hideElement(event) {
-  event.target.classList.add('hidden');
-}
-
-// To delete an element
-function deleteElement(event) {
-  event.target.remove();
-}
-
-// Add event to elements
-elements.forEach((elem) => {
-  elem.addEventListener('click', hideElement);
-  elem.addEventListener('dblclick', deleteElement);
+/*
+let simulaProceso = new Promise((resolve, reject) => {
+  let num1 = 42;
+  let num2 = 42;
+  if (num1 === num2) resolve("They are equal!");
+  else reject(Error("Something went wrong"));
 });
 
-// Function to show elements again
-function showElements() {
-  document.querySelectorAll('.hidden').forEach((elem) => {
-    elem.classList.remove('hidden');
-  });
-}
+let promise2 = new Promise((resolve, reject) =>  {
+  let num1 = 42;
+  let num2 = 73;
+  if (num1 === num2) resolve("They are equal!");
+  else reject(Error("Something went wrong"));
+});
 
-// Reaparecer button event
-mainBtn.addEventListener('click', showElements);
+promise1.then((answer) => {
+  prom1.innerHTML += answer;
+}).catch((error) => {
+  prom1.innerHTML += error.message;
+})
+
+promise2.then((answer) => {
+  prom2.innerHTML += answer;
+}).catch((error) => {
+  prom2.innerHTML += error.message;
+});
+*/
+
+function simulaProceso(exito) {
+  return new Promise((resolve, reject) => {
+    if (exito) {
+      resolve("Success!");
+    } else {
+      reject("Failed!");
+    }
+  });
+};
+
+// Test then-catch
+simulaProceso(true)
+  .then(resultado => prom1.innerHTML += resultado)
+  .catch(error => prom1.innerHTML += error);
+
+simulaProceso(false)
+  .then(result => prom2.innerHTML += result)
+  .catch(error => prom2.innerHTML += error);
+
+// Test async-await
+async function testProcess() {
+  try {
+    let successResult = await simulaProceso(true);
+    prom3.innerHTML += successResult;
+  } catch (error) {
+    prom3.innerHTML += error;
+  }
+};
+
+async function testProcess2() {
+  try {
+    let failedResult = await simulaProceso(false);
+    prom4.innerHTML += successResult;
+  } catch (error) {
+    prom4.innerHTML += error;
+  }
+};
+
+testProcess();
+testProcess2();
