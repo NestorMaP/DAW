aa5167e60e484809c938e74c633565d24c025efc
NestorMaP <mateoprats.nestor@gmail.com>
2024-11-21 22:52:33 +0100

    DWEC UD10 Practs

--- Git-2º/DWEC/UD10/js/pr1.js
+++ Git-2º/DWEC/UD10/js/pr1.js
@@ -1,11 +1,42 @@
-pictureDiv = document.getElementById('picture');
+// Same as ej1.js
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() +  (exdays * 24 * 60 * 60 *1000));
+  const expires = "expires=" + d.toUTCString();
+  document.cookie = `${cname}=${cvalue}; ${expires}; path=/`;
+}
 
-document.addEventListener('keydown', function (event) {
-  // Alt + F12 keys
-  if (event.altKey && event.key === 'F12') {
-    pictureDiv.style.backgroundImage = "url('https://picsum.photos/1920/1080')";
-
-    // Hide h1
-    document.getElementById('main_text').style.display = 'none';
+function getCookie(cname) {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(';');
+  
+  for (let cookie of cookieArray) {
+    cookie = cookie.trim();
+    if (cookie.startsWith(name)) {
+      return cookie.substring(name.length);
+    }
   }
-});
+  return "";
+}
+
+function countVisits() {
+  let counter = parseInt(getCookie('visitCounter') || '0');
+
+  counter++;
+
+  // Reset afther 10 visits
+  if (counter > 10) {
+    counter = 1;
+    document.cookie = 'visitCounter=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/';
+  }
+
+  // Save new counter
+  setCookie('visitCounter', counter.toString(), 30);
+
+  //Show on screen
+  document.getElementById('visits').textContent = counter;
+}
+
+//Execute on loading the page
+window.onload = countVisits;
--- Git-2º/DWEC/UD10/js/pr2.js
+++ Git-2º/DWEC/UD10/js/pr2.js
@@ -1,60 +1,21 @@
-let emailInput = document.getElementById('email');
-let passwordInput = document.getElementById('password');
-let submitBtn = document.getElementById('submitBtn');
-let emailError = document.getElementById('email_error');
-let passwordError = document.getElementById('password_error');
-let form = document.getElementById('form');
+function createTimer(ms) {
+  return new Promise((resolve, reject) => {
+    let primaryTimer = setTimeout(() => {
+      resolve('Time concluded!');
+    }, ms);
 
-// Email validation
-function validateEmail() {
-  let emailRegexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  return emailRegexp.test(emailInput.value);
+    let rejectTimer = setTimeout(() => {
+      clearTimeout(primaryTimer);
+      reject('The time is not correct...');
+    }, ms*2);
+  })
 }
 
-// Password validation
-function validatePassword() {
-  return passwordInput.value.length >= 8 && passwordInput.value.length <= 10;
-}
-
-// Show/hide error messages
-function showEmailError() {
-  if (!validateEmail()) {
-    emailError.style.display = 'block';
-  } else {
-    emailError.style.display = 'none';
-  }
-}
-
-function showPasswordError() {
-  if (!validatePassword()) {
-    passwordError.style.display = 'block';
-  } else {
-    passwordError.style.display = 'none';
-  }
-}
-
-// Alter submit button
-function toggleSubmitButton() {
-  submitBtn.disabled = !(validateEmail() && validatePassword());
-}
-
-// Blur and input events for email
-emailInput.addEventListener('blur', showEmailError);
-emailInput.addEventListener('input', () => {
-  emailError.style.display = 'none';
-  toggleSubmitButton();
-});
-
-// Blur and input events for password
-passwordInput.addEventListener('blur', showPasswordError);
-passwordInput.addEventListener('input', () => {
-  passwordError.style.display = 'none';
-  toggleSubmitButton();
-});
-
-// Allow submit only if validations are OK
-form.addEventListener('submit', function (event) {
-  if (submitBtn.disabled) {
-    event.preventDefault();
-  }
-});
+// Function with 5 seconds
+createTimer(5*1000)
+  .then(result => {
+    document.getElementById('result').textContent = result;
+  })
+  .catch(e => {
+    document.getElementById('result').textContent = e;
+  });
